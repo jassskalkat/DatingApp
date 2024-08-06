af018e474fa9f65c8c594e3be542e8568ec31924
jassskalkat <japneetkalkat@outlook.com>
2024-08-05 21:12:37 -0400

    updated files

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd64fb4212f64cc4cfd4f757cc481df1b4419966")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7edc244abcffc841f5143fd35b089a07b14269")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
